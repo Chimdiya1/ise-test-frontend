e033a486b360518487b8b441061dcc67438f4d5b
Chimdia Anyiam <fortunechimdiya@gmail.com>
2023-09-26 20:38:25 +0100

    dashboard link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center p-24"></main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      Go to{" "}
+      <Link href="/dashboard" className="underline font-semibold">
+        Dashboard
+      </Link>
+    </main>
+  );
 }
